16d5d41d1878995dd96b9475334964255451b899
Algoritma Dwi Febri Fathiyakan <icoreforce87@gmail.com>
2024-05-07 10:58:52 +0700

    feat: login, register, sidebar

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,7 +1,7 @@
 <div class="d-flex align-items-center justify-content-between">
     <a href="index.html" class="logo d-flex align-items-center">
-        <img src="assets/img/logo.png" alt="">
-        <span class="d-none d-lg-block">NiceAdmin</span>
+        <img src="assets/img/new.jpg" alt="">
+        {{-- <span class="d-none d-lg-block">Muslimah Bakery</span> --}}
     </a>
     <i class="bi bi-list toggle-sidebar-btn"></i>
 </div><!-- End Logo -->
@@ -165,8 +165,7 @@
 
         <li class="nav-item dropdown pe-3">
 
-            <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#"
-                data-bs-toggle="dropdown">
+            <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
                 <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
                 <span class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
             </a><!-- End Profile Iamge Icon -->

3c431bdcce73396d060b9d288e8557671f33d238
Algoritma Dwi Febri Fathiyakan <icoreforce87@gmail.com>
2024-05-20 11:47:57 +0700

    menambahkan pop up kategrori admin

--- app/Models/Produk.php
+++ app/Models/Produk.php
@@ -5,10 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Kategori extends Model
+class Produk extends Model
 {
     use HasFactory;
-
-    protected $guarded = ['id'];
+    protected $guarded = [
+        'id',
+    ];
 }
-
